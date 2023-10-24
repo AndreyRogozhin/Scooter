962e4530663d316660c8b054380d48147bdfa7f4
Andrey Rogozhin <andrey.rogozhin.2023@yandex.ru>
2023-10-24 23:59:55 +0300

    Добавлен тест списка вопросов и ответов

--- src/test/java/TestFAQList.java
+++ src/test/java/TestFAQList.java
@@ -1,19 +1,23 @@
 import org.example.MainPage;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class TestFAQList {
 
     @Parameterized.Parameter(0)
-    public int locator;
+    public int number;
     @Parameterized.Parameter(1)
-    public int textQuestion;
+    public int questionText;
     @Parameterized.Parameter(2)
-    public int textAnswer;
+    public int answerExpected;
 
 
 
@@ -31,8 +35,7 @@ public class TestFAQList {
     @Parameterized.Parameters(name = "Тест {index}: на вопрос (1) нужно получить ответ (2)")
     public static Object[][] data() {
         return new Object[][] {
-                { 2, 4, 8, 14 },
-                { 1, 1, 5, 7 }
+                { 0, "Сколько это стоит? И как оплатить?", "Сутки — 400 рублей. Оплата курьеру — наличными или картой."  },
         };
     }
 
@@ -42,9 +45,20 @@ public class TestFAQList {
         public void RunTestFAQList()  {
 
             MainPage objMainPage = new MainPage(driver);
-
             objMainPage.scrollToFAQList();
+
+            By question = By.id("accordion__heading-" + number);
+            By answer = By.id("accordion__panel-" + number);
+
+            driver.findElement(question).getText(); // не то получается
+            assertEquals("Не тот ответ!", answerExpected, answerFound);
+            }
 
 
         }
+
+        // "accordion__heading-" + number ( или использовать Format )
+    // "accordion__panel-" + number ( или использовать Format )
+
+
 }
